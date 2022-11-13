1df6c05bc19d32d1dd974558e875383f4b09e6ed
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-11-13 15:20:05 +0100

    Initial Commit

---
+++ ContentTransitionP2/ContentTransitionP2App.swift
@@ -0,0 +1,17 @@
+//
+//  ContentTransitionP2App.swift
+//  ContentTransitionP2
+//
+//  Created by Michele Manniello on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ContentTransitionP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
